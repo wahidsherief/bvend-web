b17971b6902d3ab8a92e92fe04d61b8ca1896a5f
Wahid <cengr.wahid@gmail.com>
2023-12-17 01:28:39 +0600

    cors issue solved 5

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -10,9 +10,9 @@ class Cors
     {
         $response = $next($request);
 
-        $response->headers('Access-Control-Allow-Origin', '*');
-        $response->headers('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-        $response->headers('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+        $response->headers->set('Access-Control-Allow-Origin', '*');
+        $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
         return $response;
     }
